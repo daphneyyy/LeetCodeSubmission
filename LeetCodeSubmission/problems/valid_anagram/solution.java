4ee9fc22af09ac27719abe02338056c78890e53a
daphneyang <xuy001@ucsd.edu>
2021-12-22 08:22:07 +0000

    Sync LeetCode submission - Valid Anagram (java)

---
+++ LeetCodeSubmission/problems/valid_anagram/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    private HashMap<Character, Integer> prequenceS = new HashMap<>();
+    private HashMap<Character, Integer> prequenceT = new HashMap<>();
+    
+    public boolean isAnagram(String s, String t) {
+        putString(prequenceS, s);
+        putString(prequenceT, t);
+        return prequenceS.equals(prequenceT);
+    }
+    
+    public void putString(HashMap<Character, Integer> h, String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (h.get(s.charAt(i)) == null) {
+                h.put(s.charAt(i), 1);
+            } else {
+                h.put(s.charAt(i), h.get(s.charAt(i)) + 1);
+            }
+        }
+    }
+}
