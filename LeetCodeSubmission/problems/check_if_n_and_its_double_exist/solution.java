76e7eec9238a7a4b3f43942516e9f1ab5d5ad1cc
daphneyang <xuy001@ucsd.edu>
2022-07-11 09:09:26 +0000

    Sync LeetCode submission - Check If N and Its Double Exist (java)

---
+++ LeetCodeSubmission/problems/check_if_n_and_its_double_exist/solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        Hashtable<Integer, Integer> ht = new Hashtable<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (ht.get(arr[i]) == null) {
+                ht.put(arr[i], 1);
+            } else {
+                ht.put(arr[i], ht.get(arr[i]) + 1);
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            int doub = arr[i] * 2;
+            if (doub == arr[i]) {
+                if (ht.get(doub) > 1) {
+                    return true;
+                }
+            } else if (ht.get(doub) != null) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
