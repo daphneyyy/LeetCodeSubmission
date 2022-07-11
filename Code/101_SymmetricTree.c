3e7832c8109dda13ceda441cc17f93a5f69ec1f8
Xuewen Yang <84886359+daphneyyy@users.noreply.github.com>
2022-07-11 02:26:29 -0700

    Create 101_SymmetricTree.c

---
+++ Code/101_SymmetricTree.c
@@ -0,0 +1,27 @@
+// 101. Symmetric Tree
+// Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q){
+    if ((p == NULL) & (q == NULL)) {
+        return true;
+    } else if ((p == NULL) | (q == NULL)) {
+        return false;
+    } else if (p->val != q->val) {
+        return false;
+    }
+    return (isSameTree(p->left, q->right) & isSameTree(p->right, q->left));
+}
+
+bool isSymmetric(struct TreeNode* root){
+    return isSameTree(root->left, root->right);
+}
