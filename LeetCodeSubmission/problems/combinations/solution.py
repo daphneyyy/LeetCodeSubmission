2e297a0a9012725d270f876e72e3a12a0bad77bf
daphneyang <xuy001@ucsd.edu>
2022-09-06 05:00:47 +0000

    Sync LeetCode submission - Combinations (python)

--- LeetCodeSubmission/problems/combinations/solution.py
+++ LeetCodeSubmission/problems/combinations/solution.py
@@ -1,15 +1,22 @@
-class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:
+class Solution(object):
+    def com_helper(self, res, num, cur, n, k):
+        if (len(cur) == k):
+            res.append(list(cur))
+            return
+        for i in range(num, n + 1):
+            cur += [i]
+            self.com_helper(res, i + 1, cur, n, k)
+            cur.pop()
+            
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
         if k == 0:
             return []
-        def com_helper(res, num, cur, n, k):
-            if (len(cur) == k):
-                res.append(cur.copy())
-                return
-            for i in range(num, n + 1):
-                cur += [i]
-                com_helper(res, i + 1, cur, n, k)
-                cur.pop()
         res = []
-        com_helper(res, 1, [], n, k)
+        self.com_helper(res, 1, [], n, k)
         return res
+        
