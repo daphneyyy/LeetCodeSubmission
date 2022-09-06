87388c2d4dd8cb1e6c7f1a974250edd5f95b6d50
daphneyang <xuy001@ucsd.edu>
2022-09-06 07:21:24 +0000

    Sync LeetCode submission - Subsets II (python)

---
+++ LeetCodeSubmission/problems/subsets_ii/solution.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    from collections import deque
+    
+    def sub_helper(self, queue, idx, nums, res):
+        for i in range(idx, len(nums)):
+            queue.append(nums[i])
+            res.update([tuple(list(queue))])
+            self.sub_helper(queue, i + 1, nums, res)
+            queue.pop()
+            
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        sort_nums = sorted(nums)
+        res = set([])
+        if len(nums) == 0:
+            return res
+        queue = deque()
+        self.sub_helper(queue, 0, sort_nums, res)
+        return [[]] + [list(i) for i in res]
+
+        
