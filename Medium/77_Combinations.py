8f3068b5da5a692e5f0ddd493ff224c0a2b00695
Xuewen Yang <84886359+daphneyyy@users.noreply.github.com>
2022-09-05 22:00:15 -0700

    Update and rename 77_Combinations.py3 to 77_Combinations.py

---
+++ Medium/77_Combinations.py
@@ -0,0 +1,23 @@
+# Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+# You may return the answer in any order.
+
+class Solution(object):
+    def com_helper(self, res, num, cur, n, k):
+        if (len(cur) == k):
+            res.append(list(cur))
+            return
+        for i in range(num, n + 1):
+            cur += [i]
+            self.com_helper(res, i + 1, cur, n, k)
+            cur.pop()
+            
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.com_helper(res, 1, [], n, k)
+        return res
+        
