ac46dd9f5f0108456c090b8f8dfdea208f5cf44f
Xuewen Yang <84886359+daphneyyy@users.noreply.github.com>
2023-04-13 15:53:16 -0700

    Update 183_CustomersWhoNeverOrder.sql

--- Easy/183_CustomersWhoNeverOrder.sql
+++ Easy/183_CustomersWhoNeverOrder.sql
@@ -1,6 +1,8 @@
-183. Customers Who Never Order
+-- 183. Customers Who Never Order
+-- Write an SQL query to report all customers who never order anything.
+-- Return the result table in any order.
+-- The query result format is in the following example.
 
-# Write your MySQL query statement below
 select customers.name as Customers
 from customers
 where customers.id not in
