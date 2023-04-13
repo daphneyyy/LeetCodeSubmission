90537a6726b5422f90a9e845e880ca480f3198f0
Xuewen Yang <84886359+daphneyyy@users.noreply.github.com>
2023-04-13 15:53:43 -0700

    Update 175_CombineTwoTables.sql

--- Easy/175_CombineTwoTables.sql
+++ Easy/175_CombineTwoTables.sql
@@ -1,8 +1,7 @@
-# Write an SQL query to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.
+-- Write an SQL query to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.
 
-# Return the result table in any order.
+-- Return the result table in any order.
 
-# Write your MySQL query statement below
 SELECT firstName, lastName, city, state
 FROM Person
 LEFT JOIN Address
