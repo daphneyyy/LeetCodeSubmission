b00e4105e36d822b1f8d29f0a50493f44370485e
Xuewen Yang <xuewenyang1@gmail.com>
2023-04-26 17:27:04 -0700

    Add 1045 sql

---
+++ Medium/1045_CustomersWhoBoughtAllProducts.sql
@@ -0,0 +1,9 @@
+-- Write an SQL query to report the customer ids from the Customer table that bought all the products in the Product table.
+-- Return the result table in any order.
+select customer_id
+from Customer
+group by customer_id
+having count(distinct product_key) = (
+        select count(product_key)
+        from Product
+    )
