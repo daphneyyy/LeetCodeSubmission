311774de35c91d6b4623be652ada1db2a26a703c
Xuewen Yang <xuewenyang1@gmail.com>
2023-05-04 15:50:38 -0700

    Modify 177 sql

--- Medium/177_NthHighestSalary.sql
+++ Medium/177_NthHighestSalary.sql
@@ -1,1 +1,13 @@
 -- Write an SQL query to report the nth highest salary from the Employee table. If there is no nth highest salary, the query should report null.
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT BEGIN RETURN (
+    select distinct salary
+    from (
+            select salary,
+                dense_rank() over (
+                    order by salary desc
+                ) as rk
+            from Employee
+        ) e
+    where e.rk = N
+);
+END
