3da16ea55382a0651e5596cf083f012d3b5bc0b9
Xuewen Yang <84886359+daphneyyy@users.noreply.github.com>
2024-07-03 00:12:32 -0700

    Create 620_NotBoringMovies.sql

---
+++ Easy/620_NotBoringMovies.sql
@@ -0,0 +1,11 @@
+-- Write a solution to report the movies with an odd-numbered ID and a description that is not "boring".
+
+-- Return the result table ordered by rating in descending order.
+
+-- The result format is in the following example.
+
+
+select *
+from cinema
+where id % 2 = 1 and description != "boring"
+order by rating desc;
