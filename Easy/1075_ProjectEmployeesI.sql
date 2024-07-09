3443980b0556234a25c576060dca04acb373040f
Xuewen Yang <84886359+daphneyyy@users.noreply.github.com>
2024-07-08 22:28:28 -0700

    Create 1075_ProjectEmployeesI.sql

---
+++ Easy/1075_ProjectEmployeesI.sql
@@ -0,0 +1,10 @@
+-- Write an SQL query that reports the average experience years of all the employees for each project, rounded to 2 digits.
+
+-- Return the result table in any order.
+
+-- The query result format is in the following example.
+
+select p.project_id, round(avg(e.experience_years), 2) as average_years
+from project p left join employee e
+on p.employee_id = e.employee_id
+group by p.project_id
