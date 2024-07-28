564f8f7ed3edf46a246a5de16a5c70cd9422eaa1
Xuewen Yang <84886359+daphneyyy@users.noreply.github.com>
2024-07-27 22:56:01 -0700

    Create 1204_LastPersonToFitInTheBus.sql

---
+++ Medium/1204_LastPersonToFitInTheBus.sql
@@ -0,0 +1,12 @@
+-- There is a queue of people waiting to board a bus. However, the bus has a weight limit of 1000 kilograms, so there may be some people who cannot board.
+
+-- Write a solution to find the person_name of the last person that can fit on the bus without exceeding the weight limit. The test cases are generated such that the first person does not exceed the weight limit.
+
+select person_name
+from (
+    select person_name, sum(weight) over (order by turn) as cum_weight
+    from queue
+    order by turn 
+) as cum_sum
+where cum_weight <= 1000
+order by cum_weight desc limit 1
