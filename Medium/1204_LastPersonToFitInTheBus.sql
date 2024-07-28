1c5cbb7b4728f65f2e87203781fdadfbc26a91d9
Xuewen Yang <84886359+daphneyyy@users.noreply.github.com>
2024-07-27 22:58:13 -0700

    Update 1204_LastPersonToFitInTheBus.sql

--- Medium/1204_LastPersonToFitInTheBus.sql
+++ Medium/1204_LastPersonToFitInTheBus.sql
@@ -1,3 +1,5 @@
+-- 1204. Last Person to Fit in the Bus
+
 -- There is a queue of people waiting to board a bus. However, the bus has a weight limit of 1000 kilograms, so there may be some people who cannot board.
 
 -- Write a solution to find the person_name of the last person that can fit on the bus without exceeding the weight limit. The test cases are generated such that the first person does not exceed the weight limit.
